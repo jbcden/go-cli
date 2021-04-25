b99efc26acecf34679439564c5ab0a0d14a771af
Jacob Chae <jbcden@gmail.com>
2021-04-25 22:54:49 +0900

    First CLI in Go
    This does not include exercises from the end of chapter 1

---
+++ wc/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// Parsing the flags provided by the user
+	flag.Parse()
+
+	// Calling the count function to count the number of wordds
+	// received from stdin and printing it out
+	fmt.Println(count(os.Stdin, *lines))
+}
+
+func count(r io.Reader, countLines bool) int {
+	// A scanner is used to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r)
+
+	if !countLines {
+		// Define the scanner split type to words (default is split by lines)
+		scanner.Split(bufio.ScanWords)
+	}
+
+	// Defining a counter
+	wc := 0
+
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++
+	}
+
+	// Return the total
+
+	return wc
+}
